42c00315b65eb524d60ee442cde348b65ae2bd2a
Joshua Johanan <josh359@gmail.com>
2025-02-19 22:53:17 -0500

    fix: Can override the base file path from the cli

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod pq;
 
 use anyhow::Context;
 use clap::{Parser, Subcommand};
-use config::{LoadArgs, PGFileLoad, PGFileLoadArgs};
+use config::{LoadArgs, PGFileLoad, PGFileLoadArgs, ProcessedPGFileLoad};
 use postgres::load_postgres;
 use pq::{get_fields, get_file_metadata, map_parquet_to_abstract, ParquetType};
 use tracing::{debug, info};
@@ -46,24 +46,28 @@ async fn main() -> Result<(), anyhow::Error> {
 
     let start = std::time::Instant::now();
 
-    let command_exec = match &cli.command {
+    let command_exec = match cli.command {
         Commands::Load(args) => {
             match std::env::var("LDRS_PG_URL").with_context(|| "LDRS_PG_URL not set") {
-                Ok(pg_url) => load_postgres_cmd(args, pg_url).await,
+                Ok(pg_url) => load_postgres_cmd(&args, pg_url).await,
                 Err(e) => Err(e),
             }
         }
         Commands::PGConfig(args) => {
             // open the file and parse the yaml
-            let pg_file_load = std::fs::read_to_string(args.file_path.clone())
-                .with_context(|| "Unable to read file")?;
-            let pg_file_load: PGFileLoad =
-                serde_yaml::from_str(&pg_file_load).with_context(|| "Unable to parse yaml")?;
+            let pg_file_load : ProcessedPGFileLoad = std::fs::read_to_string(args.config_path.clone())
+                .with_context(|| "Unable to read file")
+                .and_then(|f| {
+                    serde_yaml::from_str::<'_, PGFileLoad>(&f)
+                        .with_context(|| "Unable to parse yaml")
+                })
+                .map(|pg_file_load| pg_file_load.merge_cli_args(args))
+                .and_then(|pg_file_load| pg_file_load.try_into())?;
+
             match std::env::var("LDRS_PG_URL").with_context(|| "LDRS_PG_URL not set") {
                 Ok(pg_url) => {
-                    let tasks = pg_file_load.parse_args()?;
-                    let total_tasks = tasks.len();
-                    for (i, pg_load) in tasks.iter().enumerate() {
+                    let total_tasks = pg_file_load.tables.len();
+                    for (i, pg_load) in pg_file_load.tables.iter().enumerate() {
                         let task_start = std::time::Instant::now();
                         info!("Running task: {}/{}", i + 1, total_tasks);
                         load_postgres_cmd(pg_load, pg_url.clone()).await?;
