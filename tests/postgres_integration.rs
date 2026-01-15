9c7bb4e91a26bfb0f087e759835586d2059786ed
Joshua Johanan <josh359@gmail.com>
2026-01-15 01:09:39 -0500

    fix(adding pg role to config load): Pg role can be set in the dest url or with the env vars, mainly LDRS_PG_ROLE.

--- tests/postgres_integration.rs
+++ tests/postgres_integration.rs
@@ -9,7 +9,6 @@ tables:
   - name: public_test.users
     filename: tests/test_data/public.users/public.users.snappy.parquet
     post_sql: create unique index if not exists unique_id_idx on {{ name }} (unique_id);
-    role: test_role
 ",
     // test without src or dest to see if the defaults work
     "
@@ -18,7 +17,6 @@ tables:
     dest: pg.drop_replace
     filename: tests/test_data/public.users/public.users.snappy.parquet
     post_sql: create unique index if not exists unique_id_idx on {{ name }} (unique_id);
-    role: test_role
 ",
     // now test truncate insert
     "
@@ -27,7 +25,6 @@ tables:
     dest: pg.truncate_insert
     filename: tests/test_data/public.users/public.users.snappy.parquet
     post_sql: create unique index if not exists unique_id_idx on {{ name }} (unique_id);
-    role: test_role
 ",
     // should be able to infer based on delete_keys
     "
@@ -54,18 +51,19 @@ tables:
 
 #[tokio::test]
 async fn test_postgres_file_drop() {
-    let _default_guard = tracing::subscriber::set_global_default(
-        tracing_subscriber::fmt::Subscriber::builder()
-            .compact()
-            .with_max_level(tracing::Level::TRACE)
-            .finish(),
-    );
+    // let _default_guard = tracing::subscriber::set_global_default(
+    //     tracing_subscriber::fmt::Subscriber::builder()
+    //         .compact()
+    //         .with_max_level(tracing::Level::TRACE)
+    //         .finish(),
+    // );
 
     let file_url = "file://";
-    let pg_url = "postgres://postgres:postgres@localhost:5432/postgres?sslmode=disable";
+    let pg_role_url =
+        "postgres://postgres:postgres@localhost:5432/postgres?sslmode=disable&role=test_role";
     let ldrs_env = vec![
         ("LDRS_SRC".to_string(), file_url.to_string()),
-        ("LDRS_DEST".to_string(), pg_url.to_string()),
+        ("LDRS_DEST".to_string(), pg_role_url.to_string()),
         (
             "LDRS_PARAM_DEL_P1_TIMESTAMP".to_string(),
             "2024-10-08T17:22:00".to_string(),
@@ -78,19 +76,52 @@ async fn test_postgres_file_drop() {
         .build()
         .unwrap();
 
+    let pg_url = "postgres://postgres:postgres@localhost:5432/postgres?sslmode=disable";
     for config in TEST_CASES {
-        let ex = create_ldrs_exec(config, &ldrs_env, &rt.handle())
-            .await
-            .unwrap();
-
-        // assert_eq!(ex.is_ok(), true);
-
         let client = create_connection(pg_url).await.unwrap();
+        let _ = client.batch_execute("DROP SCHEMA IF EXISTS public_test CASCADE");
+        let ex = create_ldrs_exec(config, &ldrs_env, &rt.handle()).await;
+
+        assert_eq!(ex.is_ok(), true);
+
         let users = client
             .query("SELECT * FROM public_test.users", &[])
             .await
             .unwrap();
         assert_eq!(users.len(), 2);
+
+        // ensure table is owned by test_role, which should be set from the url
+        let owner_query = "
+                   SELECT tableowner
+                   FROM pg_tables
+                   WHERE schemaname = 'public_test' AND tablename = 'users'
+               ";
+        let owner_row = client.query_one(owner_query, &[]).await.unwrap();
+        let owner: String = owner_row.get(0);
+        assert_eq!(owner, "test_role", "Table should be owned by test_role");
     }
+    tokio::runtime::Handle::current().spawn_blocking(move || drop(rt));
+}
+
+#[tokio::test]
+async fn test_postgres_env_role() {
+    let file_url = "file://";
+    let pg_url = "postgres://postgres:postgres@localhost:5432/postgres?sslmode=disable";
+    let ldrs_env = vec![
+        ("LDRS_SRC".to_string(), file_url.to_string()),
+        ("LDRS_DEST".to_string(), pg_url.to_string()),
+        (
+            "LDRS_PARAM_DEL_P1_TIMESTAMP".to_string(),
+            "2024-10-08T17:22:00".to_string(),
+        ),
+        ("LDRS_PG_ROLE".to_string(), "non_existent_role".to_string()),
+    ];
+    let rt = tokio::runtime::Builder::new_multi_thread()
+        .worker_threads(1)
+        .enable_all()
+        .build()
+        .unwrap();
+    let ex = create_ldrs_exec("", &ldrs_env, &rt.handle()).await;
+    assert!(ex.is_err());
     tokio::runtime::Handle::current().spawn_blocking(move || drop(rt));
 }
